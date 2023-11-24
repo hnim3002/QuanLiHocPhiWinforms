8cc0a5e4291a10b5858cb76dc23c56de3264eaab
MinkNguyen <hnim30022003@gmail.com>
2023-11-25 01:29:25 +0700

    update

--- SignUp.Designer.cs
+++ SignUp.Designer.cs
@@ -28,6 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tenKhoaComboBox = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rePasswordTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.signUpBtn = new System.Windows.Forms.Button();
             this.signInBtn = new System.Windows.Forms.Button();
             this.passwordTxt = new System.Windows.Forms.TextBox();
@@ -35,144 +41,237 @@
             this.accountTxt = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.rePasswordTxt = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.tenKhoaComboBox = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // signUpBtn
+            // panel1
             // 
-            this.signUpBtn.Location = new System.Drawing.Point(562, 431);
-            this.signUpBtn.Name = "signUpBtn";
-            this.signUpBtn.Size = new System.Drawing.Size(174, 45);
-            this.signUpBtn.TabIndex = 14;
-            this.signUpBtn.Text = "Sign Up";
-            this.signUpBtn.UseVisualStyleBackColor = true;
-            this.signUpBtn.Click += new System.EventHandler(this.signUpBtn_Click);
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(343, 613);
+            this.panel1.TabIndex = 0;
             // 
-            // signInBtn
+            // panel2
             // 
-            this.signInBtn.Location = new System.Drawing.Point(341, 431);
-            this.signInBtn.Name = "signInBtn";
-            this.signInBtn.Size = new System.Drawing.Size(174, 45);
-            this.signInBtn.TabIndex = 13;
-            this.signInBtn.Text = "Sign In";
-            this.signInBtn.UseVisualStyleBackColor = true;
-            this.signInBtn.Click += new System.EventHandler(this.signInBtn_Click);
-            // 
-            // passwordTxt
-            // 
-            this.passwordTxt.Location = new System.Drawing.Point(358, 303);
-            this.passwordTxt.Name = "passwordTxt";
-            this.passwordTxt.Size = new System.Drawing.Size(339, 22);
-            this.passwordTxt.TabIndex = 12;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(287, 306);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 16);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "Password";
-            // 
-            // accountTxt
-            // 
-            this.accountTxt.Location = new System.Drawing.Point(358, 250);
-            this.accountTxt.Name = "accountTxt";
-            this.accountTxt.Size = new System.Drawing.Size(339, 22);
-            this.accountTxt.TabIndex = 10;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(287, 253);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 16);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Account";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(342, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(320, 91);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Sign Up";
-            // 
-            // rePasswordTxt
-            // 
-            this.rePasswordTxt.Location = new System.Drawing.Point(358, 358);
-            this.rePasswordTxt.Name = "rePasswordTxt";
-            this.rePasswordTxt.Size = new System.Drawing.Size(339, 22);
-            this.rePasswordTxt.TabIndex = 16;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(287, 361);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 16);
-            this.label4.TabIndex = 15;
-            this.label4.Text = "Password";
+            this.panel2.Controls.Add(this.tenKhoaComboBox);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.rePasswordTxt);
+            this.panel2.Controls.Add(this.accountTxt);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.signUpBtn);
+            this.panel2.Controls.Add(this.passwordTxt);
+            this.panel2.Controls.Add(this.signInBtn);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(343, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(546, 613);
+            this.panel2.TabIndex = 1;
             // 
             // tenKhoaComboBox
             // 
             this.tenKhoaComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.tenKhoaComboBox.FormattingEnabled = true;
-            this.tenKhoaComboBox.Location = new System.Drawing.Point(381, 183);
+            this.tenKhoaComboBox.Location = new System.Drawing.Point(125, 227);
             this.tenKhoaComboBox.Name = "tenKhoaComboBox";
             this.tenKhoaComboBox.Size = new System.Drawing.Size(290, 24);
-            this.tenKhoaComboBox.TabIndex = 18;
+            this.tenKhoaComboBox.TabIndex = 29;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(307, 183);
+            this.label5.Location = new System.Drawing.Point(122, 208);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(68, 16);
-            this.label5.TabIndex = 17;
+            this.label5.TabIndex = 28;
             this.label5.Text = "Tên Khoa:";
+            // 
+            // rePasswordTxt
+            // 
+            this.rePasswordTxt.Location = new System.Drawing.Point(125, 410);
+            this.rePasswordTxt.Name = "rePasswordTxt";
+            this.rePasswordTxt.Size = new System.Drawing.Size(290, 22);
+            this.rePasswordTxt.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(124, 388);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 16);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Nhập Lại Mật Khẩu";
+            // 
+            // signUpBtn
+            // 
+            this.signUpBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.signUpBtn.FlatAppearance.BorderSize = 0;
+            this.signUpBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signUpBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signUpBtn.ForeColor = System.Drawing.Color.White;
+            this.signUpBtn.Location = new System.Drawing.Point(183, 472);
+            this.signUpBtn.Name = "signUpBtn";
+            this.signUpBtn.Size = new System.Drawing.Size(174, 45);
+            this.signUpBtn.TabIndex = 25;
+            this.signUpBtn.Text = "Đăng Ký";
+            this.signUpBtn.UseVisualStyleBackColor = false;
+            // 
+            // signInBtn
+            // 
+            this.signInBtn.FlatAppearance.BorderSize = 0;
+            this.signInBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signInBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signInBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.signInBtn.Location = new System.Drawing.Point(183, 532);
+            this.signInBtn.Name = "signInBtn";
+            this.signInBtn.Size = new System.Drawing.Size(174, 45);
+            this.signInBtn.TabIndex = 24;
+            this.signInBtn.Text = "Đăng Nhập";
+            this.signInBtn.UseVisualStyleBackColor = true;
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Location = new System.Drawing.Point(125, 345);
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.Size = new System.Drawing.Size(290, 22);
+            this.passwordTxt.TabIndex = 23;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Mật Khẩu";
+            // 
+            // accountTxt
+            // 
+            this.accountTxt.Location = new System.Drawing.Point(125, 286);
+            this.accountTxt.Name = "accountTxt";
+            this.accountTxt.Size = new System.Drawing.Size(290, 22);
+            this.accountTxt.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Tên Tài Khoản";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.label1.Location = new System.Drawing.Point(133, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 69);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Đăng Ký";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(219, 392);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 28);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "System";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(54, 350);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(270, 28);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "  Tuition Management ";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(119, 303);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(195, 28);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "Welcome to the";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp2.Properties.Resources.logo10;
+            this.pictureBox1.Location = new System.Drawing.Point(59, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 218);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 30;
+            this.pictureBox1.TabStop = false;
             // 
             // SignUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1036, 589);
-            this.Controls.Add(this.tenKhoaComboBox);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.rePasswordTxt);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.signUpBtn);
-            this.Controls.Add(this.signInBtn);
-            this.Controls.Add(this.passwordTxt);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.accountTxt);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(889, 613);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "SignUp";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.SignUp_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button signUpBtn;
-        private System.Windows.Forms.Button signInBtn;
-        private System.Windows.Forms.TextBox passwordTxt;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox accountTxt;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox rePasswordTxt;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.ComboBox tenKhoaComboBox;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox rePasswordTxt;
+        private System.Windows.Forms.TextBox accountTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button signUpBtn;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.Button signInBtn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
