1de2d71e4edf9f09d7aa1e5afdd2c383a7f15617
phamnam2003 <namphamhai7@gmail.com>
2023-11-30 23:37:47 +0700

    check empty enter ChiTietHoaDon

--- ChiTietHoaDon/OpenChiTietHoaDon.Designer.cs
+++ ChiTietHoaDon/OpenChiTietHoaDon.Designer.cs
@@ -37,9 +37,10 @@
             // 
             // openBtn
             // 
-            this.openBtn.Location = new System.Drawing.Point(578, 273);
+            this.openBtn.Location = new System.Drawing.Point(434, 222);
+            this.openBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.openBtn.Name = "openBtn";
-            this.openBtn.Size = new System.Drawing.Size(132, 42);
+            this.openBtn.Size = new System.Drawing.Size(99, 34);
             this.openBtn.TabIndex = 0;
             this.openBtn.Text = "Chi Tiết Hóa Đơn";
             this.openBtn.UseVisualStyleBackColor = true;
@@ -48,39 +49,44 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(515, 188);
+            this.label1.Location = new System.Drawing.Point(386, 153);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 16);
+            this.label1.Size = new System.Drawing.Size(100, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nhập Mã Hóa Đơn ";
             // 
             // textBoxMaHD
             // 
-            this.textBoxMaHD.Location = new System.Drawing.Point(642, 185);
+            this.textBoxMaHD.Location = new System.Drawing.Point(482, 150);
+            this.textBoxMaHD.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxMaHD.Name = "textBoxMaHD";
-            this.textBoxMaHD.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMaHD.Size = new System.Drawing.Size(76, 20);
             this.textBoxMaHD.TabIndex = 2;
+            this.textBoxMaHD.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.enter_print_CTHD);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 385);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 313);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1351, 338);
+            this.dataGridView1.Size = new System.Drawing.Size(1013, 275);
             this.dataGridView1.TabIndex = 3;
             // 
             // OpenChiTietHoaDon
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1375, 735);
+            this.ClientSize = new System.Drawing.Size(1031, 597);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBoxMaHD);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.openBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "OpenChiTietHoaDon";
             this.Text = "OpenChiTietHoaDon";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
